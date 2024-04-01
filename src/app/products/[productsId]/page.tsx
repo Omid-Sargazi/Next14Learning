ad7b0257752f7aaa4a84376fb3cdf5ea18940bd8
OmidSargazi <om.sargazi@gmail.com>
2024-04-02 02:13:18 +0330

    Routing Metadata

--- src/app/products/[productsId]/page.tsx
+++ src/app/products/[productsId]/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Metadata } from 'next'
 
-export default function ProductsDetails({params}:{
-    params:{productsId:string}
-}) {
+type Props = {
+    params:{
+        productsId:string
+    }
+}
+export const generateMetaData = ({params}:Props):Metadata =>{
+    return {
+        // title: `Product ${params.productsId}`
+        title: "`Product ${params.productsId}`"
+    }
+}
+
+export default function ProductsDetails({params,}:Props) {
   return (
     <div>ProductsDetails {params.productsId}</div>
   )
