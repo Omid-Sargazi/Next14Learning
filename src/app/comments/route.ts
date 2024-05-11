260ccdee090a91f5148fd4a5808879f81556914e
OmidSargazi <om.sargazi@gmail.com>
2024-05-11 19:27:31 +0330

    Handling POST Request

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -4,3 +4,17 @@ export async function GET()
 {
     return Response.json(comments);
 }
+
+export async function POST(request:Request)
+{
+    const res = await request.json();
+    const newComment = {
+        id:comments.length+1,
+        text:res.text,
+    }
+    comments.push(newComment);
+    return new Response(JSON.stringify(newComment),{
+        headers:{"Content-Type":"application/json"},
+        status:201,
+    });
+}
