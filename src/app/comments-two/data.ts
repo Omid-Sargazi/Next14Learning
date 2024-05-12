d157bed88a6fc663087b3779918768b7e425333b
OmidSargazi <om.sargazi@gmail.com>
2024-05-12 21:20:20 +0330

    POST, GET and Dynamic Route Handlers

---
+++ src/app/comments-two/data.ts
@@ -0,0 +1,26 @@
+export const comments = [
+    {
+        id:1,
+        text:"This is a first comment"
+    },
+    {
+        id:2,
+        text:"This is a second comment"
+    },
+    {
+        id:3,
+        text:"This is a third comment"
+    },
+    {
+        id:4,
+        text:"This is a firth comment"
+    },
+    {
+        id:5,
+        text:"This is a fifth comment"
+    },
+    {
+        id:6,
+        text:"This is a sixth comment"
+    },
+]
