0a70885d8d17ad6a9e14536a5b9698f52e224256
OmidSargazi <om.sargazi@gmail.com>
2024-05-14 02:09:34 +0330

    Next.js 14 URL Query Parameters

--- src/app/comments-two/route.ts
+++ src/app/comments-two/route.ts
@@ -1,8 +1,12 @@
+import { NextRequest } from "next/server";
 import {comments} from "./data";
 
-export async function GET()
+export async function GET(request:NextRequest)
 {
-    return Response.json(comments);
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get('query');
+    const filteredComments = query ? comments.filter(commnet=>commnet.text.includes(query)):comments;
+    return Response.json(filteredComments);
 }
 
 export async function POST(request:Request)
